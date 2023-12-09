0880bfcc6e7ab0aa435133245afc9c09d80bfcf1
Brian Werner <brianmw199@gmail.com>
2023-12-08 19:45:23 -0600

    added the movie details route in app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList';
+import MovieDetails from '../MovieDetails/MovieDetails';
 
 function App() {
   return (
@@ -10,8 +11,11 @@ function App() {
         <Route path="/" exact>
           <MovieList />
         </Route>
+        <Route path="/details" exact>
+          <MovieDetails />
+        </Route>
         
-        {/* Details page */}
+        
 
         {/* Add Movie page */}
         
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,15 +1,19 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector, useHistory } from 'react-redux';
 import './MovieList.css'
 
 function MovieList() {
 
   const dispatch = useDispatch();
   const movies = useSelector(store => store.movies);
+  const history = useHistory()
 
   useEffect(() => {
     dispatch({ type: 'FETCH_MOVIES' });
   }, []);
+
+  // To load to the details page
+  history.push('/details')
 
   return (
     <main>
